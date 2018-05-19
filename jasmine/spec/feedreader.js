4e2b592854f7eda3daf2a6232fca0f01d32303b1
Claudia <hello@allegro-online.net>
2018-05-19 14:21:33 +0200

    Refactor code descriptions for feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -7,42 +7,41 @@
 $(function() {
     
 
-    /* RSS Test Suite - This suite is all about the RSS feeds definitions, the allFeeds variable in our application. */
+    /* RSS Test Suite
+     * This suite is all about the RSS feeds definitions, 
+     * the allFeeds variable in our application. 
+     */
     describe('RSS Feeds', () => {
 
         /* Test 1: Is the object "allFeed" defined and contains items, otherwise app will not run  */
-        it('are defined', () => {
+        it('-> are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
         /* Test 2: Simple "allFeeds" structure check */
-        it('is an object', () => {
+        it('-> is an object', () => {
             expect(typeof allFeeds).toBe('object');
         });
 
 
         const regexUrl = /^(http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/; 
-        // helperfunction - check if the "allFeeds" items have URL structure
+        // helperfunction - check if all the "allFeeds" items have an URL structure - uses regexp as a simple check
         const url = (array, property) => array.every(element => regexUrl.test(element[property]) === true);
 
-        /* Test 3: Every feed has an URL defined and is not empty */
-        it('it has a URL defined and URL is not empty', () => {
+        /* Test 3: Every feed has an URL defined and the URL is not empty  */
+        it('-> it has a URL defined and URL is not empty', () => {
             expect(url(allFeeds,'url')).toBe(true);
         });
 
-      
-        const regexName = /^\w+( +\w+)*$/;
-        // helperfunction - simple check if the name contains only "reasonable characters", e.g. no eMail address
-        const name = (array, property) => array.every(element => regexName.test(element[property]) === true);
+        
         // helperfunctions - check if every array element is defined and has a length > 0
         const isDefined = (array, property) => array.every(element => element[property] !== undefined);
         const hasLength = (array, property) => array.every(element => element[property].length > 0);
 
-        /* Test 4: every feed has a name property defined with reasonable characters and is not empty */
-        it('it has a name defined and name is not empty', () => {
-            expect(name(allFeeds,'name')).toBe(true);
+        /* Test 4: Every feed has a name property defined with reasonable characters and is not empty */
+        it('-> it has a name defined and name is not empty', () => {
             expect(isDefined(allFeeds,'name')).toBe(true);
             expect(hasLength(allFeeds, 'name')).toBe(true);
         });
@@ -50,15 +49,16 @@ $(function() {
     });
 
 
-
-    /* The Menu Test Suite - is the menu working correctly when clicked? */
+    /* The Menu Test Suite
+     * is the menu working correctly when clicked? 
+     */
     describe('The menu', () => {
 
         // select the body tag 
         const menu =  $('body'); 
 
         /* Test 1: Does the body tag has the class ".menu-hidden" after initial page load? */
-        it('should be hidden by default', () => {
+        it('-> should be hidden by default', () => {
             expect(menu.hasClass('menu-hidden')).toBe(true);
         }); 
 
@@ -66,7 +66,7 @@ $(function() {
          /* Test 2: Does the toggeling work? First click shows the menu, second click hides the menu? */
         const menuIcon = $('.menu-icon-link');
 
-        it('toggle menu when menu icon is clicked', () => {
+        it('-> toggle menu when menu icon is clicked', () => {
             //first click
             menuIcon.click();
             expect(menu.hasClass('menu-hidden')).toBe(false);
@@ -79,7 +79,10 @@ $(function() {
     });      
 
 
-    /* Initial Entries Test Suite - when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container. */
+    /* Initial Entries Test Suite
+     * when the loadFeed function is called and completes its work, 
+     * there is at least a single .entry element within the .feed container. 
+     */
     describe('Initial Entries', () => {
 
         /* Start loadFeed before the following tests */ 
@@ -91,7 +94,7 @@ $(function() {
 
 
         /* Test 1: is there at least one Feed Entry after the loadFeed request? */
-        it('Feed Container contains at least one Feed Entry', (done) => {
+        it('-> Feed container contains at least one feed entry', (done) => {
             // const "containerElements" needs to be inside the "it-function" in order to be declared at the right time
             const containerElements = $('.feed .entry-link');
 
@@ -102,7 +105,10 @@ $(function() {
     });
 
 
-    /* New Feed Selection Test Suite - when a new feed is loaded by the loadFeed function the content actually changes. */
+    /* New Feed Selection Test Suite
+     * when a new feed is loaded by the loadFeed function, 
+     * the content actually changes. 
+     */
     describe('New Feed Selection', (done) => {
     
         let tempBefore = "";
@@ -122,9 +128,9 @@ $(function() {
 
 
         /* Test 1: If both strings are the same, no real loadFeed took place */
-        it('load Feed function actually changes feed content', () => {
+        it('-> loadFeed function actually changes feed content', () => {
             const check = tempBefore.localeCompare(tempAfter) ;
-            /* Strings should differ, e.g., not be 0 */  
+            /* Strings should differ, e.g., not be 0, the validation does not rely on possible output -1 or 1 or other pos. or neg. values*/  
             expect(check).not.toBe(0);  
         });  
     });
